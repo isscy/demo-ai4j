dc5aac1b12bed4eba5eb73aecba420b03f47aa48
dotff <dotff@qq.com>
2025-06-05 15:34:10 +0800

    init

---
+++ src/main/java/cn/ff/demoai4j/DemoAi4jApp.java
@@ -0,0 +1,14 @@
+package cn.ff.demoai4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAi4jApp {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAi4jApp.class, args);
+    }
+
+}
